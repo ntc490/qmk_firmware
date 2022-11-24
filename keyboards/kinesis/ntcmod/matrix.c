a6d65da5d216ed687cb56d5ef278a54c5bdb0811
Nathan Crapo <nathan.crapo@axellio.com>
2022-11-24 15:21:52 -0700

    Merge a few years of changes from master into my branch

--- keyboards/kinesis/ntcmod/matrix.c
+++ keyboards/kinesis/ntcmod/matrix.c
@@ -37,8 +37,34 @@ static uint8_t matrix_row_debounce[MATRIX_ROWS];
 static uint8_t read_row(uint8_t row);
 static void select_row(uint8_t row);
 
+__attribute__ ((weak))
 void matrix_scan_kb(void) {
     matrix_scan_user();
+}
+
+__attribute__ ((weak))
+void matrix_scan_kb(void) {
+    matrix_scan_user();
+}
+
+__attribute__ ((weak))
+void matrix_init_user(void) {
+}
+
+__attribute__ ((weak))
+void matrix_scan_user(void) {
+}
+
+inline
+uint8_t matrix_rows(void)
+{
+    return MATRIX_ROWS;
+}
+
+inline
+uint8_t matrix_cols(void)
+{
+    return MATRIX_COLS;
 }
 
 void matrix_init(void)
@@ -83,12 +109,6 @@ uint8_t matrix_scan(void)
     return 1;
 }
 
-bool matrix_is_modified(void)
-{
-    if (debouncing) return false;
-    return true;
-}
-
 inline
 bool matrix_is_on(uint8_t row, uint8_t col)
 {
@@ -109,15 +129,6 @@ void matrix_print(void)
         pbin_reverse(matrix_get_row(row));
         print("\n");
     }
-}
-
-uint8_t matrix_key_count(void)
-{
-    uint8_t count = 0;
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        count += bitpop16(matrix_row[row]);
-    }
-    return count;
 }
 
 static matrix_row_t read_row(uint8_t row)
