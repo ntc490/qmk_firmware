cd0d3428a65b0f4b2bdf3b9e73f3908486f00d61
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2025-12-01 21:04:12 -0700

    Simplify sections of the config we don't need

--- keyboards/handwired/kubocho/config.h
+++ keyboards/handwired/kubocho/config.h
@@ -21,26 +21,9 @@
  * Serial driver configuration - RP2040 uses PIO-based serial
  * The "vendor" driver in info.json enables the RP2040 PIO implementation
  */
-#define SERIAL_USART_DRIVER PIO     // Use PIO for serial communication
 #define SERIAL_PIO_USE_PIO0         // Use PIO0 peripheral
 #define SERIAL_USART_TIMEOUT 100    // USART driver timeout (ms)
 #define SERIAL_USART_SPEED 38400    // Baud rate for serial communication
-
-/*
- * For RP2040 PIO-based serial, we may need to specify which PIO state machine to use
- * Uncomment if needed:
- * #define SERIAL_USART_TX_PAL_MODE 0
- */
-
-/* USB polling interval */
-#define USB_POLLING_INTERVAL_MS 1
-
-/* Debounce reduces chatter (unintended double-presses) */
-#define DEBOUNCE 5
-
-/* Matrix configuration - handled by direct pins in info.json */
-#define MATRIX_ROWS 8  // 4 rows per side
-#define MATRIX_COLS 6  // 6 columns per side
 
 /*
  * Tapping configuration for home row mods
